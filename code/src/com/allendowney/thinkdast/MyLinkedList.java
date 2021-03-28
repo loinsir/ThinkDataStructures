c3d9ebb9082291fdeeb4db0208df56d62158017c
loinsir <a9327370@gmail.com>
2021-03-28 14:28:06 +0000

    3.28

--- code/src/com/allendowney/thinkdast/MyLinkedList.java
+++ code/src/com/allendowney/thinkdast/MyLinkedList.java
@@ -83,6 +83,13 @@ public class MyLinkedList<E> implements List<E> {
 	@Override
 	public void add(int index, E element) {
 		//TODO: FILL THIS IN!
+        if (index == 0) {
+            head = new Node(element, head);
+        } else {
+            Node node = getNode(index-1);
+            node.next = new Node(element, node.next);
+        }
+        size++;
 	}
 
 	@Override
@@ -144,6 +151,12 @@ public class MyLinkedList<E> implements List<E> {
 	@Override
 	public int indexOf(Object target) {
 		//TODO: FILL THIS IN!
+        Node node = head;
+        for(int index=0; index<size; index++, node = node.next) {
+            if (equals(target, node.data)) {
+                return index;
+            }
+        }
 		return -1;
 	}
 
@@ -209,7 +222,16 @@ public class MyLinkedList<E> implements List<E> {
 	@Override
 	public E remove(int index) {
 		//TODO: FILL THIS IN!
-		return null;
+        E element = get(index);
+        
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node node = getNode(index-1);
+            node.next = node.next.next;
+        }
+        size--;
+        return element;
 	}
 
 	@Override
